cea88f6fab3a2a395bd0bef35ff45fa0f713f4ac
carlosmartde <160431626+carlosmartde@users.noreply.github.com>
2024-05-16 14:58:41 -0600

    QUE NUNMERO ES MAYOR

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,24 @@
 public class Main {
     public static void main(String[] args) {
         //DETERMINAR SI ES MAYOR DE EDAD O NO
-        int edad;
+        /*int edad;
         System.out.println("Ingrese su edad");
         edad = Integer.parseInt(System.console().readLine());
         if (edad >= 18) {
             System.out.println("Si es mayor de edad");
         }else if (edad < 18) {
             System.out.println("No es mayor de edad");
+        }*/
+        //DETERMINAR QUE NUMERO ES MAYOR
+        float a,b;
+        System.out.println("Ingrese el primer numero: ");
+        a = Float.parseFloat(System.console().readLine());
+        System.out.println("Ingrese el segundo numero: ");
+        b = Float.parseFloat(System.console().readLine());
+        if(a>b){
+            System.out.println(a+" Es mayor que "+b);
+        }else {
+            System.out.println(b+"Es mayor que "+a);
         }
     }
 }

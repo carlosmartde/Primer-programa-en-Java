a497bad21187927ecb1eb5e86619f665a2062c49
carlosmartde <160431626+carlosmartde@users.noreply.github.com>
2024-05-17 10:54:12 -0600

    NUMEROS IMPARES USANDO WHILE

--- src/Main.java
+++ src/Main.java
@@ -48,6 +48,30 @@ public class Main {
         }
         System.out.println("El factorial de "+numero+" es: "+factorial);*/
         //NUMEROS IMPARES
+        /*
+        Scanner scanner = new Scanner(System.in);
+        int numero = 0;
+        while (true) {
+            System.out.print("Por favor, ingrese un número mayor a 10 y menor que 30: ");
+            if (scanner.hasNextInt()) {
+                numero = scanner.nextInt();
+                if (numero > 10 && numero < 30) {
+                    break;
+                } else {
+                    System.out.println("El número debe ser mayor a 10 y menor que 30. Intente de nuevo.");
+                }
+            } else {
+                System.out.println("Entrada no válida. Por favor, ingrese un número.");
+                scanner.next(); // Descarta la entrada no válida
+            }
+        }
+        System.out.println("Números impares desde 1 hasta " + numero + ":");
+        for (int i = 1; i <= numero; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+        scanner.close();*/
         Scanner scanner = new Scanner(System.in);
         int numero = 0;
 
@@ -69,12 +93,15 @@ public class Main {
 
         // Mostrar los números impares desde 1 hasta el número ingresado
         System.out.println("Números impares desde 1 hasta " + numero + ":");
-        for (int i = 1; i <= numero; i++) {
+        int i = 1;
+        while (i <= numero) {
             if (i % 2 != 0) {
                 System.out.println(i);
             }
+            i++;
         }
 
         scanner.close();
     }
-}
+
+    }
